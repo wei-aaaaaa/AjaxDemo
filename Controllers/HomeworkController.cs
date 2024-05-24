7015cb11fbc35ccb9c94073c92c9a27dd942822f
wei-aaaaaa <stanley05232930@gmail.com>
2024-05-24 21:33:36 +0800

    加入專案檔案。

---
+++ Controllers/HomeworkController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSIT158Site.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult Homework1()
+        {
+            return View();
+        }
+
+        public IActionResult CheckAccount()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/MemberDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MSIT158Site.Models
+{
+    public class MemberDTO
+    {
+        public string? userName { get; set; }
+
+        public string? Email { get; set; }
+
+        public int? Age { get; set; }
+    }
+}
+//chalgfuiklj dhhkkhhs oodakhff fcak,sdhhak jklj>csdgj/ 
